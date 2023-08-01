f5928eba72f7d69bb9c38cc3313172e1a5c7d057
Elena Gerasimchuk <gerasimchukee@alderasoft.ru>
2023-08-01 14:12:59 +0700

    Initial Commit

---
+++ Study-Jam-Summary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study-Jam-Summary
+//
+//  Created by Елена on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
